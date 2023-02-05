f8552a85cf33ac9b959a83716d47a3d078017ec9
sveldi <veldi@amazon.com>
2023-02-05 16:27:29 -0600

    Externalize input and output CSV prefixes to ENV

--- singleJob/src/singlejob.py
+++ singleJob/src/singlejob.py
@@ -14,18 +14,19 @@ job_id = os.getenv('AWS_BATCH_JOB_ID')
 s3_bucket = os.getenv('S3_BUCKET_NAME')
 
 # Read all input CSV files in chunks of 1 line
-input_csv_path = 's3://' + s3_bucket + '/csv-input/'
+input_csv_prefix = os.getenv('S3_INPUT_CSV_PREFIX')
+input_csv_path = 's3://' + s3_bucket + '/' + input_csv_prefix
 dfs = wr.s3.read_csv(path=input_csv_path, chunksize=1)
 
 
 # Write output CSV files
-output_csv_prefix = 's3://' + s3_bucket + '/csv-output/'
+output_csv_prefix = os.getenv('S3_OUTPUT_CSV_PREFIX')
 i=1
 for csv_df in dfs:
-    output_csv_path = output_csv_prefix + str(i) + '.csv'
+    output_csv_file_path = 's3://' + s3_bucket + '/' + output_csv_prefix + str(i) + '.csv'
     wr.s3.to_csv(
         df=csv_df,
-        path=output_csv_path,
+        path=output_csv_file_path,
     )
     i = i + 1
 
